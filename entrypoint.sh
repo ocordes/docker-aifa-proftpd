222bf058c3023b9ad8db5eaea4fb3cb21a586692
Oliver Cordes <ocordes@astro.uni-bonn.de>
2018-09-06 11:18:21 +0200

    - change the entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+env
+
+PROFTPD_ARGS="-DMOD_EXEC=$MOD_EXEC -DMOD_TLS=$MOD_TLS -DMOD_VROOT=$MOD_VROOT -DVERBOSE=$FTP_VERBOSE"
+
+if [ "$MASQ_ADDR" = "AWS" ]; then
+    MASQ_ADDR=`curl -f -s http://169.254.169.254/latest/meta-data/public-ipv4`
+fi
+
+if [ ! -z "$MASQ_ADDR" ]; then
+    PROFTPD_ARGS="$PROFTPD_ARGS -DUSE_MASQ_ADDR"
+fi
+
+
+echo $PWD_SALT > /etc/proftpd/.salt
+
+# allow proftpd writing custom logs
+chown -R proftpd:proftpd /var/log/proftpd
+
+exec /usr/local/sbin/proftpd --nodaemon $PROFTPD_ARGS
